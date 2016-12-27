50128ffa9917b90ee99c2a97e0a467ff33ff1e42
Inna Shpota <shpota_inna@ukr.net>
2016-12-27 22:39:53 +0200

    Fixed folder creation in bat files

--- compile-tests.bat
+++ compile-tests.bat
@@ -1,3 +1,4 @@
+if not exist .\build\test-classes md .\build\test-classes
 javac -d ./build/test-classes ^
 -classpath "lib/*;build/tasks.jar" ^
 -sourcepath tests tests/ua/shpota/tasks/model/*.java
--- docs.bat
+++ docs.bat
@@ -1,3 +1,4 @@
+if not exist .\doc md .\doc 
 javadoc -d ./doc -sourcepath ^
 sources sources/ua/shpota/tasks/controller/TaskController.java ^
         sources/ua/shpota/tasks/model/*.java ^
